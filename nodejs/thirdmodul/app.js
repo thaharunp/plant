716be4490c46d7e220ddd2d6f1e633a62c6f3276
sadhakunps <sadaqunps@gmail.com>
2024-04-24 15:35:10 +0530

    express

---
+++ nodejs/thirdmodul/app.js
@@ -0,0 +1,13 @@
+
+import dotenv from 'dotenv'
+
+dotenv.config({'path':'./fahad.env'})
+
+let port = process.env.PORT 
+let host =process.env.HOST 
+let key = process.env.SKEY
+
+
+console.log(port)
+console.log(host)
+console.log(key)
